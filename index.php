63e378858ae62231c0330b76370d63321334b4b9
jimmygeyns@gmail.com <jimmy@ubuntu.(none)>
2014-01-28 15:37:48 +0100

    initial push, start-of-website

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>VM pro Team</title>
+	<!--insert styles-->
+	<link rel="stylesheet" href="./css/myCss.css">
+</head>
+<body>
+	<div id="container">
+		<!--<h1>Vanmeerbeeck een staande traditie</h1>-->
+		<div id="left">
+			<a href="#"><img src="./img/jen.png" alt=""></a>
+		</div>
+		<div id="right">
+			<a href="#"><img src="./img/steve.png" alt=""></a>
+		</div>
+		<div id="mid">
+			<a href="#"><img src="./img/paint.png" alt=""></a>
+		</div>
+	</div>
+</body>
+</html>
